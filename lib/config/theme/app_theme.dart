eaad7a7ccdac2b9890a63910d3ddeaec6fb91ef5
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-11-13 20:55:18 -0600

    app theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.deepPurple,
+  Colors.deepOrange,
+  Colors.pink,
+  Colors.black
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'selected color must be greater then 0');
+
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colorList[selectedColor]);
+}
