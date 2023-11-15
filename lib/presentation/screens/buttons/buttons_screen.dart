84ecd2e6f2139a90df3318a7051b8ce18ae5dd74
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-11-15 14:02:19 -0600

    diferentes botones

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -9,7 +10,67 @@ class ButtonScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Button Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonView(),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () {
+            context.pop();
+          }),
+    );
+  }
+}
+
+class _ButtonView extends StatelessWidget {
+  const _ButtonView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: Wrap(
+        spacing: 10,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Elevated'),
+          ),
+          const ElevatedButton(
+            onPressed: null,
+            child: Text('Elevated Disable'),
+          ),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.access_alarm_rounded),
+              label: const Text('Elevated icon')),
+          FilledButton(
+            onPressed: () {},
+            child: const Text('Filled'),
+          ),
+          FilledButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.adb_outlined),
+            label: const Text('Filled icon'),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('Outline'),
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.add_ic_call),
+            label: const Text('Outline icon'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Text'),
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.add_to_home_screen_outlined),
+            label: const Text('Text icon'),
+          ),
+        ],
+      ),
     );
   }
 }
