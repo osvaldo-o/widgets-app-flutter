b238ff31accd7aab9199fec5a50a7dcd91147b53
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-11-25 12:21:02 -0600

    progress cointrolled linear and circular

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -30,7 +30,7 @@ class _ProgressView extends StatelessWidget {
             backgroundColor: Colors.black38,
           ),
           SizedBox(height: 20),
-          Text('Circular indicator controlado'),
+          Text('Circular y Linear controlado'),
           SizedBox(height: 10),
           _ControlledProgressIndicator(),
         ],
@@ -44,6 +44,28 @@ class _ControlledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+        stream: Stream.periodic(
+                const Duration(milliseconds: 300), (value) => (value * 2) / 100)
+            .takeWhile((value) => value < 100),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0;
+          return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(
+                    value: progressValue,
+                    strokeWidth: 2,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                      child: LinearProgressIndicator(
+                    value: progressValue,
+                  ))
+                ],
+              ));
+        });
   }
 }
